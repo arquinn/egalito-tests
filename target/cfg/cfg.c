c46fcb059018fd05626fd56bf6da942e2b61d0ea
Hidenori <hidenori.kobayashi@gmail.com>
2017-09-25 23:20:45 -0400

    Fix compile warning.

--- target/cfg/cfg.c
+++ target/cfg/cfg.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     int i;
     if(argc & 1) {
         for(i = 0; i < argc; i++) {
-            printf("%d\n", argv[i]);
+            printf("%s\n", argv[i]);
         }
     }
 
