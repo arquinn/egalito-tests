c9c673448d2f7e9056c211164bec4561fcaf6a2d
Hidenori <hidenori.kobayashi@gmail.com>
2017-11-12 14:45:39 -0500

    Add test program regarding environ.
    This covers the case of copy relocation + emulation.

---
+++ target/environ/environ.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main(int argc, char **argv, char **envp) {
+    for(int i = 0; environ[i]; i ++) {
+        printf("environ[%d] = \"%s\"\n", i, environ[i]);
+    }
+
+    printf("note: envp is %lx\n", (unsigned long)envp);
+    printf("note: *envp is %s\n", *envp);
+    printf("getenv: PATH=%s\n", getenv("PATH"));
+    puts("iterating through envp manually:");
+    for(char **env = envp; *env; env ++) {
+        puts(*env);
+    }
+    return 0;
+}
